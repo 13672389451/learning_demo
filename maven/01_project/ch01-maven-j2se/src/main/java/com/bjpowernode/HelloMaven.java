085680795e7df949f735b6a177c0b8d8db25e9cc
Brett <oncwnuId1Qimgh6Lrgab9INo9f2s@git.weixin.qq.com>
2022-09-02 10:32:11 +0800

    Maven 学习

---
+++ maven/01_project/ch01-maven-j2se/src/main/java/com/bjpowernode/HelloMaven.java
@@ -0,0 +1,12 @@
+package com.bjpowernode;
+
+public class HelloMaven {
+    public static int add(int n1,int n2){
+        return n1+n2;
+    }
+
+    public static void main(String args[]){
+        add(10,20);
+        System.out.println(add(10,20));
+    }
+}
---
+++ maven/01_project/ch01-maven-j2se/src/test/java/com/bjpowernode/testHello.java
@@ -0,0 +1,12 @@
+package com.bjpowernode;
+
+import org.junit.Test;
+
+public class testHello {
+    @Test
+    public void test(){
+        HelloMaven helloMaven = new HelloMaven();
+        System.out.println(helloMaven.add(12,20));
+
+    }
+}
